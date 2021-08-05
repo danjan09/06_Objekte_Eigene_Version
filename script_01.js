a73dc2e00474b3a20d0436d99045323a95191641
danjan09 <janzen.daniel@gmail.com>
2021-08-05 15:02:22 +0200

    Donnerstag

--- script_01.js
+++ script_01.js
@@ -2,37 +2,60 @@
 /***** Erinnerung: Arrays *******/
 // let arr;
 // arr = ["Ich","bin","Max"];
-// ausgabe(arr);
-// ausgabe(arr[2]);
+// output(arr);
+// output(arr[2]);
 
 // arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
-// ausgabe(arr[0][2]);
-// ausgabe(arr[1][2]);
+// output(arr[0][2]);
+// output(arr[1][2]);
+
+
+
+/***** Objekte 2 Hierarchie *******/
+
+let baikal ={
+        wert:"10",
+        deep:{
+                deeper:{
+                        deepest:"Das Licht - auf 1642m!"
+                }
+        }
+}
+
+output(baikal.wert);
+output(baikal.deep.deeper.deepest);
 
 
 /***** Objekte 1 Daten/Funktionen *******/
 
 let person = 
 {
-        firstName:"Maurice",
+        firstName:"Max",
         familyName:"König",
         salary:[12000,16000],
-        hasPermission: true
+        hasPermission: true,
+        sayHello:
+                function(){
+                        return  "Hi, ich bin " + this.familyName + "!";  // this -> von innen zugreifen
+                }
 };
 
-ausgabe(person);
-ausgabe(person.firstName);
-ausgabe(person.familyName);
-ausgabe(person.salary[0]);
-ausgabe(person.hasPermission);
+
+
+// output(person);
+// output(person.firstName);
+// output(person.familyName);
+// output(person.salary[0]);
+// output(person.hasPermission);
+// output(person.sayHello());
 
 const txt =     "Ich bin " + person.firstName + " " +
                 person.familyName + " und verdiene " +
                 person.salary[1] + " p.a."
-ausgabe(txt);
+// output(txt);
 
 
 /** Ausgabe */
-function ausgabe(outputStr) {
+function output(outputStr) {
         console.log(outputStr);
 }
