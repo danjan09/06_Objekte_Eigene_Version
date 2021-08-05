6bc9daa2f54df8e19e0fd4995d5c2439e827a0f3
Peter <schmidt.multimedia@gmail.com>
2021-08-05 13:02:03 +0200

    01 / Objekte | properties

--- script_01.js
+++ script_01.js
@@ -1,8 +1,8 @@
 
-
 /***** Erinnerung: Arrays *******/
 // let arr;
 // arr = ["Ich","bin","Max"];
+// ausgabe(arr);
 // ausgabe(arr[2]);
 
 // arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
@@ -12,6 +12,24 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = 
+{
+        firstName:"Maurice",
+        familyName:"König",
+        salary:[12000,16000],
+        hasPermission: true
+};
+
+ausgabe(person);
+ausgabe(person.firstName);
+ausgabe(person.familyName);
+ausgabe(person.salary[0]);
+ausgabe(person.hasPermission);
+
+const txt =     "Ich bin " + person.firstName + " " +
+                person.familyName + " und verdiene " +
+                person.salary[1] + " p.a."
+ausgabe(txt);
 
 
 /** Ausgabe */
