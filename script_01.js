ab20e2555abafbad3c41963ee72527f798c425ac
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-08-05 12:20:36 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,20 @@
+
+
+/***** Erinnerung: Arrays *******/
+// let arr;
+// arr = ["Ich","bin","Max"];
+// ausgabe(arr[2]);
+
+// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
+// ausgabe(arr[0][2]);
+// ausgabe(arr[1][2]);
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+
+
+/** Ausgabe */
+function ausgabe(outputStr) {
+        console.log(outputStr);
+}
